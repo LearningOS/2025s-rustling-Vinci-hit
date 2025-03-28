9efb82de0e55b0a9901e57efec13b32240278c6c
Vinci <24s051007@stu.hit.edu.cn>
2025-03-28 09:23:15 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -31,8 +31,9 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+    let mut t = address  as *mut u32;
     unsafe {
-        todo!("Your code goes here")
+        *t = 0xAABBCCDD;
     }
 }
 
